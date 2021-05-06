0f5adfc51af08669972cdebdf87c255ff4953e9b
Abishek B <abishekbalaji7.151@gmail.com>
2021-05-06 07:12:48 +0530

    Read new deck from hard drive added

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
-	cards := newDeck()
-	cards.saveToFile("cards.txt")
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
-	fmt.Println(cards.toString())
+	cards := newDeckFromFile("cards.txt")
+	cards.print()
 }
